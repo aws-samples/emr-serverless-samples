56bc9cbf4b7dbb77be6295af85983629f3c99f33
Damon P. Cortesi <d.lifehacker@gmail.com>
2022-08-17 16:34:40 -0700

    Add some integration tests

---
+++ airflow/tests/test_dag_integrity.py
@@ -0,0 +1,12 @@
+"""Test the validity of all DAGs."""
+
+from airflow.models import DagBag
+
+
+def test_dagbag():
+    """
+    Validate DAG files using Airflow's DagBag.
+    This includes sanity checks e.g. do tasks have required arguments, are DAG ids unique & do DAGs have no cycles.
+    """
+    dag_bag = DagBag(include_examples=False)
+    assert not dag_bag.import_errors  # Import errors aren't raised but captured to ensure all DAGs are parsed
